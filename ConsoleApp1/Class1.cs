dd506383f912ae53fdbcec1300afb7d605016a8e
YeyePPT <yeye92713@gmail.com>
2024-04-08 14:05:54 +0800

    just a test

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//为什么命名空间可以一样，而类名不可以一样？
+//1、类型所在的命名空间主要用于标识类型所在的程序集，所以内部可以使用共同的标识
+//2、namespace 命名空间，可以被多类使用，标识这些类都在同一个命名空间下
+//3、同一个命名空间不允许出现相同的类型名称，不同的命名空间可以出现相同的类型名称
+
+namespace A
+{
+    class Class1
+    {
+        public void Display()
+        {
+            Console.WriteLine("Class1");
+        }
+    }
+    class Class2
+    {
+        public void Display()
+        {
+            Console.WriteLine("Class2");
+        }
+    }
+}
+
+namespace A
+{ 
+    class Class3 
+    {
+        public void Display()
+        {
+            Console.WriteLine("Class3");
+        }
+    }
+    class Class4
+    {
+        public void Display()
+        {
+            Console.WriteLine("Class4");
+        }
+    }
+}
+
+namespace B
+{ 
+    class Class1
+    {
+        public void Display()
+        {
+            Console.WriteLine("Class5");
+        }
+    }
+    class Class2
+    {
+        public void Display()
+        {
+            Console.WriteLine("Class6");
+        }
+    }
+}
+
+namespace SetUp
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("start with me");
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace Test
+{
+    class A
+    {
+        public static void ADD(int a , int b)
+        {
+            Console.WriteLine(a + b);
+        }
+     
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program                //为什么main函数需要静态的？？？
+    {
+        static void Main(string[] args) //因为程序运行初期，内存中没有任何对象，这个时候要运行必须有一个起始点，而静态是作为程序初始化默认分配的内存空间，所以直接调取。
+        {
+            Console.Write("hello,World"); //这是行注释，
+            Console.ReadLine();
+            /*  这是块注释
+             * 
+             * 
+             * 
+             * 
+             * 
+             */
+            Console.WriteLine("sdvds");
+            Console.ReadLine(); 
+            StringBuilder stuff = new StringBuilder();
+            new A.Class1().Display();//有重名的类，需要完全限定
+            new B.Class1().Display();
+            Console.ReadLine();
+        }
+    }
+} 
+//启动项可以改！！！
+
